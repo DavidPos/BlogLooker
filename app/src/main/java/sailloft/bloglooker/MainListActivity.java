be0ef1cf656046844c0376ffa001243d9d28a092
David Pos <bigdavepos@aol.com>
2014-10-22 19:39:29 -0700

    Commit

--- app/src/main/java/sailloft/bloglooker/MainListActivity.java
+++ app/src/main/java/sailloft/bloglooker/MainListActivity.java
@@ -1,29 +1,33 @@
 package sailloft.bloglooker;
 
-import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.app.ListActivity;
-import android.widget.Toast;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mAndroidNames;
+    protected String[] mBlogPostFiles;
+    public static final int NUMBER_OF_POSTS = 20;
+    public static final String TAG = MainListActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
-        Resources resources = getResources();
-        mAndroidNames = resources.getStringArray(R.array.android_names);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-        setListAdapter(adapter);
+        try {
+            URL blogFeedUrl = new URL("http://blog.treehouse.com/api/get_recent_summary/?count=20");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         //Toast.makeText(this, getString(R.string.no_item), Toast.LENGTH_LONG).show();
     }
